f80435544c3b82266b9e8414cbf6dfa1c27f37ec
Petra Baranski <info@progsource.de>
2020-03-28 01:23:30 +0100

    Player can move left and right

--- code/scripts/Player.gd
+++ code/scripts/Player.gd
@@ -1,10 +1,31 @@
-extends RigidBody2D
+extends KinematicBody2D
 
+
+export(float, 10, 300, 0.01) var speed : float = 40.0
+
+
+var is_paused : bool = false
+var direction : float = 0.0
 
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-#func _process(delta):
-#	pass
+func _input(_event):
+	if is_paused:
+		return
+	
+	direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+
+func _physics_process(delta):
+	if is_paused:
+		return
+
+	var force_x : float = direction * speed * delta
+	var force = Vector2(force_x, 0)
+	var collision = move_and_collide(force)
+	
+	if collision:
+		print("collided")
