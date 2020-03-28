f8776102cc9784d7023fe9e4bf265548cc303d57
teemitzitrone <agata.raap@posteo.de>
2020-03-28 09:23:59 +0100

    toiletpaper script

---
+++ code/scripts/Toiletpaper.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+func _ready():
+	add_to_group("item")
+	# warning-ignore:return_value_discarded
+	EventBus.connect("item_picked_up", self, "_on_item_picked_up")
+
+func _on_item_picked_up(sender_id) -> void:
+	if get_instance_id() == sender_id:
+		queue_free()
+
+
+func _on_Toiletpaper_body_entered(body):
+	if body.name == "Player":
+		EventBus.emit_signal("item_picked_up", get_instance_id())
