3ba7d2d21d566b808628c8f61d6108fd4a357d3a
Petra Baranski <info@progsource.de>
2020-03-28 09:51:46 +0100

    Added main game script

---
+++ code/scripts/scenes/Game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+
+func _ready():
+	print("show start popup")
+	print("load level")
+
+
+
+func _process(delta):
+	print("make level move based on player")
+
+
+func _on_start_button_pressed():
+	print("start the game")
