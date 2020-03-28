0d2e1794e7ed6eaaca40df78228036057e5ea970
Petra Baranski <info@progsource.de>
2020-03-28 02:19:32 +0100

    While player jumps, he cannot change direction

--- code/scripts/Player.gd
+++ code/scripts/Player.gd
@@ -1,13 +1,16 @@
 extends KinematicBody2D
 
 
-export(int) var run_speed = 100
+export(int) var run_speed = 200
 export(int) var jump_speed = -400
 export(int) var gravity = 1200
+export(float, 0, 1, 0.1) var friction = 0.1
+export(float, 0, 1, 0.1) var acceleration = 0.5
 
 var is_paused : bool = false
 var velocity = Vector2()
 var jumping = false
+var direction : float = 0.0
 
 
 func _ready():
@@ -18,22 +21,29 @@ func _ready():
 func _input(_event):
 	if is_paused:
 		return
-	
-	var direction : float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.x = direction * run_speed
-	
+
+	velocity.x = 0
+
 	var jump = Input.is_action_just_pressed('ui_select')
 	
 	if jump and is_on_floor():
 		jumping = true
 		velocity.y = jump_speed
+	
+	if is_on_floor():
+		direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+	if direction != 0:
+		# accelerate when there's input
+		velocity.x = lerp(velocity.x, direction * run_speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
 
 
 func _physics_process(delta):
 	if is_paused:
 		return
 
-	#velocity.x *= delta
 	velocity.y += gravity * delta
 	
 	if jumping and is_on_floor():
