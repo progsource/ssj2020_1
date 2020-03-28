c05598a2ea8cc738d9d379e9fa923769f17732c1
Petra Baranski <info@progsource.de>
2020-03-28 22:40:18 +0100

    Background: fix objects suddenly disappearing

--- code/scripts/BackgroundObstacle.gd
+++ code/scripts/BackgroundObstacle.gd
@@ -3,7 +3,7 @@ extends Sprite
 
 func _ready():
 	# warning-ignore:return_value_discarded
-	$VisibilityNotifier2D.connect("screen_exited", self, "_on_screen_exit")
+	$VisibilityNotifier2D.connect("viewport_exited", self, "_on_screen_exit")
 
 
 func _on_screen_exit():
