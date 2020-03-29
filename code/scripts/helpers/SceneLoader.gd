000533cb83d985b4ae1cfc214d850c194114968f
Petra Baranski <info@progsource.de>
2020-03-29 12:02:02 +0200

    You can die

--- code/scripts/helpers/SceneLoader.gd
+++ code/scripts/helpers/SceneLoader.gd
@@ -1,14 +1,20 @@
 extends CanvasLayer
 
+
 onready var animation_player := $AnimationPlayer
 
+
 export(float, 0.5, 1.0, 0.1) var delay = 0.5
+
 
 func _ready() -> void:
 	# warning-ignore:return_value_discarded
 	EventBus.connect("objective_completed", self, "_on_objective_completed")
+	# warning-ignore:return_value_discarded
+	EventBus.connect("infected", self, "_on_infected")
 
-func _on_objective_completed(_sender_id, path) -> void:
+
+func _on_objective_completed(_sender_id, path) -> void :
 	if get_tree().get_current_scene().has_node("Menu"):
 		var menu = get_tree().get_current_scene().get_node("Menu")
 		menu.set_visible(false)
@@ -19,3 +25,7 @@ func _on_objective_completed(_sender_id, path) -> void:
 	animation_player.play_backwards("fade")
 	yield(animation_player, "animation_finished")
 	EventBus.emit_signal("scene_changed", get_instance_id())
+
+
+func _on_infected(_sender_id) -> void :
+	_on_objective_completed(_sender_id, "res://scenes/GameOver.tscn")
