68245229c8e8802bc272531856830169c0e808f2
Petra Baranski <info@progsource.de>
2020-03-29 16:15:49 +0200

    more level

---
+++ code/scripts/LoadingScene.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+
+func _ready():
+	# warning-ignore:return_value_discarded
+	$Timer.connect("timeout", self, "_on_timer_timeout")
+
+
+func _on_timer_timeout():
+	get_tree().change_scene("res://scenes/GameStart.tscn")
